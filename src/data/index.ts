bd2e09dceaf7144df18bc10bdca8dfa641523d17
Woojin Song <wsong31@my.bcit.ca>
2025-08-30 12:33:22 -0700

    Update barrel exports

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,7 @@
 export { personalInfo } from './personalData';
-export { projects } from './projectsData';
+export { projectsCards } from './projectsData';
 export { aboutCards } from './aboutCardsData';
 export { contactCards } from './contactCardsData';
 export { skillsCards } from './skillsCardsData';
 
-export type { Category, Project, Card, PersonalInfo } from '../types/types';
+export type { Category, Card, PersonalInfo } from '../types/types';
