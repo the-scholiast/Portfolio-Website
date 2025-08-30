fde7f3213d5a14400fb7b4a7510a19a9d50640ad
Woojin Song <wsong31@my.bcit.ca>
2025-08-30 12:33:05 -0700

    Remove Project type and use Card type for all

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,17 +1,9 @@
 export type Category = 'about me' | 'projects' | 'contact' | 'skills';
 
-export interface Project {
-  id: string;
-  title: string;
-  description: string;
-  technologies: string[];
-  githubUrl?: string;
-  liveUrl?: string;
-}
-
 export interface Card {
   id: string;
   name?: string;
+  type?: string;
   proficiency?: number;
   yearsOfExperience?: number;
   description?: string;
